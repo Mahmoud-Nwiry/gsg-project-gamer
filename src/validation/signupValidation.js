50cac4d6fcc63421b5978a8040a26f01c37df8c3
Mahmoud-Nwiry <mamo992001@gmail.com>
2022-12-29 11:28:31 +0200

    edit login schema remove regex from password && redesign the schemas (login and signup)

--- src/validation/signupValidation.js
+++ src/validation/signupValidation.js
@@ -1,12 +1,20 @@
 import * as yup from "yup";
 
 const signupSchema = yup.object().shape({
-    email: yup.string().email().required(),
-    password: yup.string().min(8).matches("^[a-zA-Z0-9!@#$&()-`.+,/\"]*$").required(),
-    password2: yup.string().when('password', (password, field) =>
-        password ? field.required().oneOf([yup.ref('password')]) : field
-    ),
-    agree: yup.boolean().required()
+    email: yup.string()
+            .email()
+            .required("Email is required"),
+    password: yup.string()
+            .min(8, "password must be 8 characters at least")
+            .matches("^[a-zA-Z0-9!@#$&()-`.+,/\"]*$")
+            .required("Password is required"),
+    password2: yup.string()
+            .when('password', (password, field) =>
+                password ? field.required().oneOf([yup.ref('password')]) : field
+            ),
+    agree:
+        yup.boolean(true)
+            .required()
 })
 
 export default signupSchema;
