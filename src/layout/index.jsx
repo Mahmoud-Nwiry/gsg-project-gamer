a8d581aa23ffc192cf74c2916b9879f0daa3b522
Mahmoud-Nwiry <mamo992001@gmail.com>
2023-01-03 19:46:07 +0200

    complete the layout (sidebard and navbar)

---
+++ src/layout/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import './layout.css'
+
+import Sidebar from '../components/Sidebar'
+import Navbar from '../components/Navbar'
+
+import Image from '../assets/images/pirsonal.png'
+
+export default class index extends Component {
+  render() {
+    return (
+      <div className='layout'>
+        <Sidebar />
+        <div className="right_side">
+            <Navbar firstName={'mahmoud'} image={Image}  />
+            {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
+import Layout from '../../layout'
 
 export default class Home extends Component {
   render() {
     return (
-      <div>Home</div>
+      <section className='home_page'>
+        <Layout>
+            
+        </Layout>
+      </section>
     )
   }
 }
