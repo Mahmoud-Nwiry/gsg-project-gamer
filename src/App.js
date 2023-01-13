58e86b86cab1905fe1db3cbde48e2e9a81861386
Mahmoud-Nwiry <mamo992001@gmail.com>
2023-01-13 16:29:10 +0200

    change app file from function to class component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
-import { useRoutes } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css';
 
 import Login from './pages/Login';
 import Signup from './pages/Signup'
 import Home from './pages/Home'; 
-import Error404 from './pages/Error404'; 
+import Error404 from './pages/Error404';
 
-const App = () => {
+class App extends Component {
 
-  let routes = useRoutes([
-    {
-      path: '/',
-      element: <Login />
-    },
-    {
-      path: '/dashboard/:id',
-      element: <Home />
-    },
-    {
-      path: '/login',
-      element: <Login />
-    },
-    {
-      path: '/signup',
-      element: <Signup/>
-    },
-    {
-      path: '*',
-      element: <Error404 />
-    }
-  ])
+  state = {
+    isAuth : true,
+    userId : null,
+  }
 
-  return routes
+  render () {
+    return (
+      <Routes>
+        <Route index element={this.state.isAuth ? <Navigate to={`/dashboard/${this.state.userId}`} /> : <Navigate to="/login" />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/dashboard/:id' element={<Home />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
+    )
+  }
 }
 
 export default App;
